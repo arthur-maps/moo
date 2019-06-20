a352a3fc488a3c729cfa02eb771a3b35faef59c5
arthur-maps <52044975+arthur-maps@users.noreply.github.com>
2019-06-20 14:34:43 -0400

    Create mapscript.js

---
+++ js/mapscript.js
@@ -0,0 +1,9 @@
+var mapboxAccessToken = 'pk.eyJ1IjoiYXJ0aHVyLW1hcHMiLCJhIjoiY2p4NHdjNXJrMDI1azQ5bTRwOW5jc2F5bSJ9.OSGinHj1JWVuEHIe91Eikw';
+var map = L.map('mapid').setView([29.7, -82.3], 6);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxAccessToken, {
+    id: 'mapbox.light',
+    attribution: "© Mapbox"
+}).addTo(map);
+
+L.geoJson(FLstrat).addTo(map);
