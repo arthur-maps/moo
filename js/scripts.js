d949730db2dacab9de1b30e5dae1b2b90ddf0bb0
arthur-maps <52044975+arthur-maps@users.noreply.github.com>
2019-06-20 12:29:35 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var mapboxAccessToken = {pk.eyJ1IjoiYXJ0aHVyLW1hcHMiLCJhIjoiY2p4NHZuem9hMGUwYTQ4bndxeHdkbmhnNiJ9.re1IdiuQLHc3Co4Wsc6Vcg};
+var mapboxAccessToken = pk.eyJ1IjoiYXJ0aHVyLW1hcHMiLCJhIjoiY2p4NHZuem9hMGUwYTQ4bndxeHdkbmhnNiJ9.re1IdiuQLHc3Co4Wsc6Vcg;
 var map = L.map('mapid').setView([37.8, -96], 4);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxAccessToken, {
