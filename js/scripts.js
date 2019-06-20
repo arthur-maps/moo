7d6334347b594d882bd7a2970b42ec27a96cf5de
arthur-maps <52044975+arthur-maps@users.noreply.github.com>
2019-06-20 12:27:56 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
+var mapboxAccessToken = {pk.eyJ1IjoiYXJ0aHVyLW1hcHMiLCJhIjoiY2p4NHZuem9hMGUwYTQ4bndxeHdkbmhnNiJ9.re1IdiuQLHc3Co4Wsc6Vcg};
+var map = L.map('mapid').setView([37.8, -96], 4);
 
-var mymap = L.map('mapid').setView([29.706, -82.34], 13);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxAccessToken, {
+    id: 'mapbox.light',
+    attribution: ...
+}).addTo(map);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={pk.eyJ1IjoiYXJ0aHVyLW1hcHMiLCJhIjoiY2p4NHY4dTA0MGFyaTQwcGN1Z2s4aG5rNyJ9.Vrd6-12La3xfYSoym0aASA}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
+L.geoJson(statesData).addTo(map);
