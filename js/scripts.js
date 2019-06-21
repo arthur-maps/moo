27ec5f5d4fab25681b6a90395665d320d75e3490
arthur-maps <52044975+arthur-maps@users.noreply.github.com>
2019-06-21 11:19:00 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,10 +7,10 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='
 }).addTo(map);
 
 
-L.esri.tiledMapLayer({
-  url: 'https://tiles.arcgis.com/tiles/cDCsY3VB02CTTRKx/arcgis/rest/services/FL_strata_WGS1984/VectorTileServer',
-  maxZoom: 15
-}).addTo(map);
+//L.esri.tiledMapLayer({
+ // url: 'https://tiles.arcgis.com/tiles/cDCsY3VB02CTTRKx/arcgis/rest/services/FL_strata_WGS1984/VectorTileServer',
+  //maxZoom: 15
+//}).addTo(map);
 
 L.esri.Vector.layer('b4d9265377b64a7e910c201fa9f3bf75').addTo(map);
 
