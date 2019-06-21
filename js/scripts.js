8b29fcde5b94ba93a00e5854b337c5d93ee15413
arthur-maps <52044975+arthur-maps@users.noreply.github.com>
2019-06-21 12:27:52 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 var mapboxAccessToken = 'pk.eyJ1IjoiYXJ0aHVyLW1hcHMiLCJhIjoiY2p4NHdjNXJrMDI1azQ5bTRwOW5jc2F5bSJ9.OSGinHj1JWVuEHIe91Eikw';
    
 
-//var map = L.map('map', {
-  //      minZoom: 5,
-    //    maxZoom: 15
-    //});
+var map = L.map('mapid', {
+        minZoom: 5,
+        maxZoom: 15
+    });
 
-var map = L.map('mapid').setView([29.7, -82.3], 7);
+//var map = L.map('mapid').setView([28.5, -81.6], 7);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxAccessToken, {
     id: 'mapbox.light',
     attribution: "© Mapbox"
 }).addTo(map);
 
-map.setView([29.7, -82.3], 7);
+map.setView([[28.5, -81.6], 7);
 
 //L.esri.tiledMapLayer({
  // url: 'https://tiles.arcgis.com/tiles/cDCsY3VB02CTTRKx/arcgis/rest/services/FL_strata_WGS1984/VectorTileServer',
